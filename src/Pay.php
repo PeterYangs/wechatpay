707942075039d32372cc260bb31357c13aa00776
PeterYangs <904801074@qq.com>
2018-05-02 13:58:34 +0800

    测试

---
+++ src/Pay.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/2 0002
+ * Time: 13:47
+ */
+
+namespace wechatPay;
+
+class Pay
+{
+
+    function __construct()
+    {
+
+        echo 123;
+    }
+
+
+}
---
+++ test/index.php
@@ -0,0 +1,5 @@
+<?php
+require '../vendor/autoload.php';
+
+$p=new \wechatPay\Pay();
+
